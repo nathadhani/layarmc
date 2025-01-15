965a9c637e5a70bc8fb190111370265d331e83ad
DmaDev <dma.develop@gmail.com>
2025-01-15 15:24:52 +0700

    'pc15012025'

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	header("Refresh:$sec;");	
 	error_reporting("E_ALL ^ E_NOTICE");	
 	$company = array('companyname' => 'PT. PERMATA VALAS UTAMA', 
-                   'address' => 'Grand Itc Permata Hijau Lt. Dasar Blok C18 No.1 | Jl. Arteri Permata Hijau, Jakarta Selatan | (021) 5366 4614 / WA : 0822 4666 7301'
+                   'address' => 'Grand Itc Permata Hijau Lt. Dasar Blok C18 No.1 <br> Jl. Arteri Permata Hijau, Jakarta Selatan | (021) 5366 4614 / WA : 0822 4666 7301'
                   );		
 ?>
 <!DOCTYPE html>
@@ -18,7 +18,7 @@
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/logopermata.png" rel="icon">
   <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
@@ -39,42 +39,25 @@
   
 
   <!-- Template Main CSS File -->  
-  <link href="assets/css/style_dashboard.css" rel="stylesheet">  
+  <link href="assets/css/style_dashboard.css" rel="stylesheet"> 
 </head>
 
 <body onLoad="setInterval('displayServerTime()', 1000);">
 
   <!-- ======= Header ======= -->
-  <header id="header" class="header fixed-top d-flex align-items-center">
+  <!-- <header id="header" class="header fixed-top d-flex align-items-center">
     <div class="d-flex align-items-center justify-content-between">
       <a href="index.html" class="logo d-flex align-items-center">
-        <img src="assets/img/logo.png" alt="">
+        <img src="assets/img/logopermata.png" alt="">
         <span class="d-none d-lg-block"><?=$company["companyname"]?></span>
+        <span class="d-none d-lg-block">Jam Operasional : Senin - Jum'at ( 8AM - 7PM) | Sabtu - Minggu ( 10AM - 7PM)</span>
       </a>      
-    </div><!-- End Logo -->       
+    </div>
     <nav class="header-right ms-auto">
-      <span style="text-align:right;font-weight:bold;font-name:arial-black;color:#000;">
-        <?php
-            function dayList(){
-              $date = date('Y-m-d');
-              $day = date('D', strtotime($date));
-              $hari = array(
-                  'Sun' => 'Minggu',
-                  'Mon' => 'Senin',
-                  'Tue' => 'Selasa',
-                  'Wed' => 'Rabu',
-                  'Thu' => 'Kamis',
-                  'Fri' => 'Jumat',
-                  'Sat' => 'Sabtu'
-              );
-              return $hari[$day];
-          }
-          echo dayList(). ', ' . date('d F Y');
-        ?>  
-        | <span id="clock" style="font-weight:bold;font-name:arial-black;color:#000;"><?php print date('H:i:s');?></span>
-      </span>      
+      
     </nav>
-  </header><!-- End Header --> 
+  </header> -->
+  <!-- End Header --> 
 
   <main id="main" class="main">
     
@@ -82,69 +65,115 @@
       <div class="row">
 
         <!-- Left side columns -->
-        <div class="col-lg-7">
-          <div class="row">           
-            <!-- Video -->
-            <div class="col-12">
+        <div class="col-lg-8">
+          <div class="row">
+            <div class="col-mb-12">
               <div class="card">
-                <div class="card-body">                  
-                  <video controls width="700" id="myVideo" autoplay muted loop>                  
-                    <source src="videoplayback.mp4" type="video/mp4"/>  
-                  </video>                  
-                </div>
+                <!-- <div class="card-body"> -->
+                  <video controls id="myVideo" autoplay muted loop>
+                    <source src="video.mp4" type="video/mp4"/>
+                  </video>
+                <!-- </div> -->
               </div>
-            </div><!-- End Video -->            
-
+            </div>
           </div>
+          <div class="row">
+            <div class="col-mb-12">
+                <div class="card mb-3">
+                  <div class="row g-0">
+                      <div class="col-md-6">
+                        <!-- <img src="assets/img/images.jpg" class="img-fluid rounded-start" alt="..."> -->
+                        <div class="card-body">
+                          <p>
+                            <strong><span style="font-size:20px;"><?=$company["companyname"]?></span></strong>
+                            <br>
+                            <?=$company["address"]?>
+                          </p>
+                        </div>                          
+                      </div>
+                      <div class="col-md-6">
+                        <div class="card-body">                          
+                          <p>
+                            <strong><span style="font-size:20px;">Jam Operasional :</span></strong>
+                            <br>
+                            Senin - Jum'at ( 8AM - 7PM) <br>
+                            Sabtu - Minggu ( 10AM - 7PM)
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>            
+                </div>
+            </div>            
         </div><!-- End Left side columns -->
 
         <!-- Right side columns -->
-        <div class="col-lg-5">
+        <div class="col-lg-4">
           <!-- Top Selling -->
-          <div class="col-12">
-          <div class="card">
-            <div class="card-body">
-              <!-- Table with stripped rows -->
-              <table id="example" class="table table-striped table-bordered" style="width:100%">
-                <thead>
-                  <tr>                    
-                    <th>Currency</th>
-                    <th>Buy</th>
-                    <th>Sale</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <?php		
-                      $txt_file = file_get_contents('file/currency.txt');
-                      $rows     = explode("\n", $txt_file);
-                      array_shift($rows);
-                      
-                      $no = 1;
-                      foreach($rows as $row => $data) {
-                        $row_data = explode('^', $data);                    
-                        $info[$row]['CURRENCY'] = $row_data[0];
-                        $info[$row]['BUY']      = $row_data[1];
-                        $info[$row]['SALE']     = $row_data[2];
-                        if($no <= 10){
-                    ?>    
-                          <tr>
-                            <td><?=$info[$row]['CURRENCY']?></td>  
-                            <td><?=$info[$row]['BUY']?></td> 
-                            <td><?= $info[$row]['SALE']?></td>
-                          </tr>                         
-                    <?php  
-                        }
-                        $no++; 
-                      } 
-                      fclose($txt_file);
-                    ?>
-                </tbody>
-              </table>
-              <!-- End Table with stripped rows -->
-
-            </div>
-          </div>
-            
+          <div class="col-md-12">
+            <div class="card">
+              <div class="card-body">       
+                <h5 class="card-title">
+                  <span style="text-align:right;font-weight:bold;font-name:arial-black;color:#000;">
+                    <?php
+                        function dayList(){
+                          $date = date('Y-m-d');
+                          $day = date('D', strtotime($date));
+                          $hari = array(
+                              'Sun' => 'Minggu',
+                              'Mon' => 'Senin',
+                              'Tue' => 'Selasa',
+                              'Wed' => 'Rabu',
+                              'Thu' => 'Kamis',
+                              'Fri' => 'Jumat',
+                              'Sat' => 'Sabtu'
+                          );
+                          return $hari[$day];
+                      }
+                      echo dayList(). ', ' . date('d F Y');
+                    ?>  
+                    - <span id="clock" style="font-weight:bold;font-name:arial-black;color:#d62d20;"><?php print date('H:i:s');?></span>
+                  </span>
+                </h5>         
+                <table id="main-table" class="table table-striped table-bordered" style="width:100%">
+                  <thead>
+                    <tr>                    
+                      <th style="text-align:center;">#</th>
+                      <th>Currency</th>
+                      <th>Buy</th>
+                      <th>Sale</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <?php		
+                        $txt_file = file_get_contents('file/currency.txt');
+                        $rows     = explode("\n", $txt_file);
+                        array_shift($rows);
+                        
+                        $no = 1;
+                        foreach($rows as $row => $data) {
+                          $row_data = explode('^', $data);                    
+                          $info[$row]['CURRENCY'] = $row_data[0];
+                          $info[$row]['BUY']      = $row_data[1];
+                          $info[$row]['SALE']     = $row_data[2];
+                          // if($no <= 10){
+                      ?>    
+                            <tr>
+                              <td style="text-align:center;"><?=$no?></td>
+                              <td><?=$info[$row]['CURRENCY']?></td>  
+                              <td style="color:blue;"><?=$info[$row]['BUY']?></td> 
+                              <td style="color:red;"><?= $info[$row]['SALE']?></td>
+                            </tr>                         
+                      <?php  
+                          // }
+                          $no++; 
+                        } 
+                        fclose($txt_file);
+                      ?>
+                  </tbody>
+                </table>                
+              </div>
+            </div>            
           </div><!-- End Top Selling -->          
         </div><!-- End Right side columns -->
 
@@ -154,16 +183,16 @@
   </main><!-- End #main -->
 
   <!-- ======= Footer ======= -->
-  <footer id="footer" class="footer">
+  <!-- <footer id="footer" class="footer">
     <div class="copyright">
-      <strong><span><?=$company["companyname"]?></span></strong>
+      <strong><span style="font-size:20px;"><?=$company["companyname"]?></span></strong>
     </div>
     <div class="credits">
-      <p>        
+      <span style="font-size:15px;">        
         <?=$company["address"]?>
-      </p>      
+      </span>      
     </div>
-  </footer>
+  </footer> -->
   <!-- End Footer -->
 
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
@@ -236,8 +265,49 @@
     ?>
   </script>
 
+  <script type="text/javascript">    
+    $(document).ready( function () {
+      var table = $('#main-table').DataTable( {
+        bLengthChange : false,
+        bInfo : false,
+        pageLength : 10,
+        lengthMenu: [[10, -1], [10, 'Todos']]
+      } )
+    } );
+  </script>
+
   <script type="text/javascript">
-    new DataTable('#example');
+    $(document).bind("contextmenu",function(e) {
+      e.preventDefault();
+    });
+    
+    $(document).keydown(function(e){
+      if(e.which === 123){
+          return false;
+      }
+    });
+    
+    document.onkeydown = function(e) {
+      if (e.ctrlKey && 
+        (e.keyCode === 67 || 
+          e.keyCode === 86 || 
+          e.keyCode === 85 || 
+          e.keyCode === 117)) {
+        return false;
+      } else {
+        return true;
+      }
+    };
+    $(document).keypress("u",function(e) {
+      if(e.ctrlKey)
+      {
+      return false;
+      }
+      else
+      {
+      return true;
+      }
+    });
   </script>
 
 </body>
